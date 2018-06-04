334588e83637febd340fa6214f1bc17602ab9f9d
Felicia Zhang <zhang.fel@husky.neu.edu>
2018-06-04 00:55:55 -0400

    initial website setup

---
+++ src/app.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+class App extends React.Component {
+  public render() {
+    return (
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">This our website</h1>
+        </header>
+        <p className="app-intro">
+          Welcome to hack beanpot 2019
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
