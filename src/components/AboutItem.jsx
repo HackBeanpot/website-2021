3f7c5ac4aac60ca836496f3f99c2ccf8590da886
Warren-Partridge <maps@perdix.org>
2018-07-05 12:59:41 -0400

    Implement floatDirection prop for AboutItem

--- src/components/AboutItem.jsx
+++ src/components/AboutItem.jsx
@@ -1,39 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Link from 'gatsby-link';
 
-class AboutItem extends Component {
-  render() {
-    if (this.props.floatDirection === 'left') {
-      return (
-        <div style={{clear: 'both'}}>
-          <img
-            src="https://hackbeanpot.com/img/wmud/IMG_0299.jpg"
-            height="40%"
-            width="40%"
-            style={{ float: 'left' }}
-          />
-          <h3>{this.props.header}</h3>
-          <h4>{this.props.content}</h4>
-          <Link to={this.props.pageLink}>Link to {this.props.pageLink}</Link>
-        </div>
-      );
-    } else if (this.props.floatDirection === 'right') {
-      return (
-        <div style={{clear: 'both'}}>
-          <img
-            src="https://hackbeanpot.com/img/wmud/IMG_0299.jpg"
-            height="40%"
-            width="40%"
-            style={{ float: 'right' }}
-          />
-          <h3>{this.props.header}</h3>
-          <h4>{this.props.content}</h4>
-          <Link to={this.props.pageLink}>Link to {this.props.pageLink}</Link>
-        </div>
-      );
-    }
-  }
-}
+const AboutItem = props => (
+  <div style={{ clear: 'both' }}>
+    <img
+      src="https://hackbeanpot.com/img/wmud/IMG_0299.jpg"
+      className={`align-${props.floatDirection}-img`}
+    />
+    <h3>{props.header}</h3>
+    <h4>{props.content}</h4>
+    <Link to={props.pageLink}>Link to {props.pageLink}</Link>
+  </div>
+);
+
 AboutItem.defaultProps = {
   header: 'AboutItem section header',
   content: 'AboutItem section content',
