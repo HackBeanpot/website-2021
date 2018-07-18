9979e7ec3191f46753494815cd879f7d1b8c85a2
feliciazhang <zhang.fel@husky.neu.edu>
2018-07-17 19:39:56 -0700

    Revert "Circle fix (#17)" (#20)
    This reverts commit 17592bdc8fce183cdd9b3117586e4a7cc08be257.

--- src/pages/stories/index.jsx
+++ src/pages/stories/index.jsx
@@ -1,25 +1,11 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import Link from 'gatsby-link';
 
-import Stories from 'pages/stories/components/stories';
-import FeaturedProjects from 'pages/stories/components/featured-projects';
+const Stories = () => (
+  <div>
+    <h1>We've all got our stories</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+);
 
-class StoriesPage extends React.Component {
-  render() {
-    return (
-      <Fragment>
-        {/* TODO: Feed in stories data */}
-        <Stories />
-
-        <div className="stories-future">
-          <h2>Our Future</h2>
-          <p>{/* TODO: Come up with content for this */}</p>
-        </div>
-
-        {/* TODO: Feed in projects data */}
-        <FeaturedProjects />
-      </Fragment>
-    );
-  }
-}
-
-export default StoriesPage;
+export default Stories;
