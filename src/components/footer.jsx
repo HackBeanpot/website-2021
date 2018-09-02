9ad03e864dfbe02f40235c62b4c83b4b527c748e
feliciazhang <zhang.fel@husky.neu.edu>
2018-09-02 13:34:59 -0700

    Replace footer placeholders (#42)

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,49 +1,42 @@
 import React from 'react';
+import Link from 'gatsby-link';
 import MailchimpSubscribe from 'react-mailchimp-subscribe';
+import InstagramIcon from 'mdi-react/InstagramIcon';
+import FacebookBoxIcon from 'mdi-react/FacebookBoxIcon';
+import SnapchatIcon from 'mdi-react/SnapchatIcon';
+import LinkedinBoxIcon from 'mdi-react/LinkedinBoxIcon';
+import LogoWhite from 'images/logo-white.jsx';
 
-const mailchimpUrl = 'todo mailchimp url';
-const cocUrl = 'todo code of conduct url';
+const mailchimpUrl =
+  'https://hackbeanpot.us9.list-manage.com/subscribe?u=13bbeb925dd0787e5b3b9b8c3&id=ce1b65ecda';
 
 const Footer = () => (
   <div className="dark-background footer">
     <div className="row">
       <div className="offset-sm-2 col-sm-2 center">
-        <img
-          className="logo-small"
-          src="https://hackbeanpot.com/img/solid_logo.png"
-        />
+        <LogoWhite />
       </div>
       <div className="col-sm-3 center-h-v">
         <span>
-          <h3>Stay Connected</h3>
+          <h3 className="footer-heading">Stay Connected</h3>
           <MailchimpSubscribe url={mailchimpUrl} />
         </span>
       </div>
       <div className="col-sm-3 center-h-v">
         <span>
-          <img
-            className="icon-small"
-            src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Ski_trail_rating_symbol-green_circle.svg"
-          />
-          <img
-            className="icon-small"
-            src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Ski_trail_rating_symbol-green_circle.svg"
-          />
-          <img
-            className="icon-small"
-            src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Ski_trail_rating_symbol-green_circle.svg"
-          />
-          <img
-            className="icon-small"
-            src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Ski_trail_rating_symbol-green_circle.svg"
-          />
+          <InstagramIcon className="icon-small" />
+          <FacebookBoxIcon className="icon-small" />
+          <SnapchatIcon className="icon-small" />
+          <LinkedinBoxIcon className="icon-small" />
         </span>
       </div>
     </div>
     <div className="center-text">
       <p>
         HackBeanpot, Inc. is a registered 501(c)(3) organization.
-        <a href="cocUrl">Code of Conduct</a>
+        <Link className="coc-link" to="/CodeOfConduct/">
+          Code of Conduct
+        </Link>
       </p>
     </div>
   </div>
