b90dfd31ee2fddd83b1c7ea6fc1f283db9d2c21a
Warren-Partridge <maps@perdix.org>
2018-09-12 11:56:16 -0400

    Make sitemap.xml generate on build

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: 'HackBeanpot 2019 - Come together to build and learn this Spring'
+    title: 'HackBeanpot 2018',
+    siteUrl: 'https://hackbeanpot.com/'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-resolve-src',
-    `gatsby-plugin-sass`
+    `gatsby-plugin-sass`,
+    'gatsby-plugin-sitemap'
   ]
 };
