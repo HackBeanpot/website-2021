e341873e4008895e7aa8387342ebabcadd1f933a
Vyshnavi <vchunduru@ccs.neu.edu>
2019-10-29 20:11:53 -0400

    moved quotes to json

--- src/pages/stories/components/stories.jsx
+++ src/pages/stories/components/stories.jsx
@@ -3,62 +3,13 @@ import Quote from 'pages/stories/components/quote';
 import Circuit1 from 'images/circuit-1';
 import SponsorBlob1 from 'images/sponsor-blob-1';
 import SponsorBlob2 from 'images/sponsor-blob-2';
+import Quotes from 'data/stories.json';
 
 class Stories extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       currentQuote: 0,
-      quotes: [
-        {
-          content:
-            'There was definitely a lot of support and direction, both with the staff and mentors from actual companies. If you wanted to do something and didn’t know how to do it, there was definitely people you could talk to that would help you learn how. HackBeanpot had a lot of people just giving talks about things in computer science that people may want to learn, and I feel like that was really cool because it was so easy to learn and pick up new things.',
-          author: 'Shane',
-          company: 'Attendee',
-          imgSrc:
-            'http://tools.hackbeanpot.com/assets/images/stories-portraits/shane.jpg'
-        },
-        {
-          content:
-            'I ... didn’t really feel at the same level as people who had been coding for a long time. I went in really scared, but while being there it was actually a really fun and enjoyable learning experience!',
-          author: 'Olivia Petrillo',
-          company: 'Attendee',
-          imgSrc:
-            'http://tools.hackbeanpot.com/assets/images/stories-portraits/olivia.jpg'
-        },
-        {
-          content:
-            'In terms of elements to bring to HackBeanpot, I think a big one is inclusivity. To the team that would mean making sure our team is inclusive of everyone’s opinions and perspectives… To the event I think diversity is important for encouraging people who don’t necessarily see themselves at a hackathon to participate and try something new.',
-          author: 'Caitlin',
-          company: 'Organizer',
-          imgSrc:
-            'http://tools.hackbeanpot.com/assets/images/stories-portraits/caitlin.jpg'
-        },
-        {
-          content:
-            'I had a really awesome time and I recommend anyone who’s interested in going to a hackathon to go to HackBeanpot. It was definitely a creative environment and even if you didn’t know much you could learn so much from the people there.',
-          author: 'Kelsey Esposito',
-          company: 'Attendee',
-          imgSrc:
-            'http://tools.hackbeanpot.com/assets/images/stories-portraits/kelsey.jpg'
-        },
-        {
-          content:
-            'If i could describe hackbeanpot in one word...invigorating. We had a lot of folks who weren’t sure what the experience would be like, and they left feeling united--Really energized and really impressed with the amount of talent that was on display as well as the atmosphere.',
-          author: 'Rob',
-          company: 'PowerAdvocate',
-          imgSrc:
-            'http://tools.hackbeanpot.com/assets/images/stories-portraits/rob.png'
-        },
-        {
-          content:
-            "HackBeanpot is the best hackathon I've been to, hands down….Being a mentor is super rewarding because I got to see a ton of projects go from the idea phase to a working demo.",
-          author: 'Sanders',
-          company: 'Past Organizer',
-          imgSrc:
-            'http://tools.hackbeanpot.com/assets/images/stories-portraits/sanders.png'
-        }
-      ]
     };
   }
 
@@ -77,7 +28,7 @@ class Stories extends React.Component {
           >
             <img
               className="portrait"
-              src={this.state.quotes[index].imgSrc}
+              src={Quotes[index].imgSrc}
               onClick={() => {
                 this.setState({ currentQuote: index });
               }}
@@ -103,7 +54,7 @@ class Stories extends React.Component {
             </div>
           </div>
           <div className="row top-images">{this.renderImages([0, 1, 2])}</div>
-          <Quote quote={this.state.quotes[this.state.currentQuote]} />
+          <Quote quote={Quotes[this.state.currentQuote]} />
           <div className="row bottom-images">
             {this.renderImages([3, 4, 5])}
           </div>
