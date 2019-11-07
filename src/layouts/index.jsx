dca6d837d26229b169b224899222e7435331625e
Caitlin Wang <caitlinwang97@gmail.com>
2019-11-06 20:09:37 -0500

    New nav and footer! (#106)
    * Create desktop nav
    
    * Create mobile nav
    
    * Cleanup nav
    
    * Create DynamicLink component and make nav links a JSON file
    
    * Reorder imports
    
    * Add basic footer
    
    * Add leaves
    
    * Update homepage about illustrations
    
    * Make mobile nav functional component
    
    * Use Pascal casing for components I touched
    
    * Revert "Use Pascal casing for components I touched"
    
    This reverts commit 84385ac28c5eb6ed636550fffd2a06fed313f4f2.

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 
-import StaticFooter from '../components/static-footer';
+import Footer from '../components/footer';
 import Nav from 'components/nav';
-import 'styles/main.scss';
+import NavMobile from 'components/nav-mobile';
+
 import favicon from '../pages/favicon.png';
+
+import '../../node_modules/@mdi/font/css/materialdesignicons.min.css';
+import 'styles/main.scss';
 
 // Shows the header and footer on every page
 const Layout = ({ children }) => (
@@ -30,8 +34,9 @@ const Layout = ({ children }) => (
       link={[{ rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }]}
     />
     <Nav />
+    <NavMobile />
     <div>{children()}</div>
-    <StaticFooter />
+    <Footer />
   </div>
 );
 
