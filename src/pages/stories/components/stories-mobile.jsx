6c505c7a5798e1fd27d59b57ba3aed80b6d8eb53
Caitlin Wang <caitlinwang97@gmail.com>
2019-11-19 18:58:58 -0500

    Fix Flickity

--- src/pages/stories/components/stories-mobile.jsx
+++ src/pages/stories/components/stories-mobile.jsx
@@ -1,33 +1,45 @@
-import React, { Fragment } from 'react'; 
+import React from 'react';
+import Flickity from 'flickity';
+
 import Quotes from 'data/quotes.json';
 import Quote from 'pages/stories/components/quote';
 
-import Flickity from 'flickity';
 import 'flickity/dist/flickity.min.css';
 
 class StoriesMobile extends React.Component {
+  componentDidMount() {
+    this.flkty = new Flickity('.stories-carousel', {
+      wrapAround: false,
+      prevNextButtons: false,
+      pageDots: true,
+      adaptiveHeight: true
+    });
+  }
 
-    renderQuotes() {
-        return Quotes.map((person, index)=> {
-            return (
-                <div key={`stories-${index}`} className="carousel-cell">
-                    <div className="stories-circles__img-container selected center" >
-                        <img className="stories-circles__img" src={person.imgSrc} alt={person.name}  />
-                    </div>
-                    <Quote quote={person} />
-                </div>
-            )
-        })
-    }
-    render() {
-        return(
-            <Fragment>
-                <div class="carousel" data-flickity="">
-                    {this.renderQuotes()}
-                </div>
-            </Fragment>
-        )
-    }
+  renderQuotes() {
+    return Quotes.map((person, index) => {
+      return (
+        <div key={`stories-${index}`} className="stories-carousel-cell">
+          <div className="stories-circles__img-container">
+            <img
+              className="stories-circles__img"
+              src={person.imgSrc}
+              alt={person.name}
+            />
+          </div>
+          <Quote quote={person} />
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <div className="stories-mobile stories-carousel">
+        {this.renderQuotes()}
+      </div>
+    );
+  }
 }
 
 export default StoriesMobile;
