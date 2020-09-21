5dff1a15984f45bf2fb36ce56c26d27dd7a8ca7a
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2020-09-20 21:05:23 -0400

    I hate everything.

--- src/pages/packet/index.jsx
+++ src/pages/packet/index.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import Layout from '../../components/layout';
+import CloudBackground from '../../images/svg/packet-cloud.svg'
 
 export default () => (
-  <Layout>
-    <SponsorPacket/>
-  </Layout>
+  <SponsorPacket />
 )
 
 class SponsorPacket extends React.Component {
   render() {
     return (
       <div className="packet">
-        <section className="packet-header">
-          <h1>
-            why doesn't this show up I hate css
-          </h1>
+        <section className="packet-cover">
+          <div className='title'>
+            <h1>
+              Sponsor
+            </h1>
+            <h1>
+              HBP 2021
+            </h1>
+            <img className="cloud-background" src={CloudBackground} alt=""/>
+            <p className="cloud-text">
+              At HackBeanpot, we aim to foster an inclusive community <br/> that
+              welcomes students of any skill level to work together <br/> on
+              engaging technical projects. By bringing local <br/>companies,
+              organizers, and attendees together, <br/> HackBeanpot celebrates
+              innovation and inclusion in Boston tech.
+            </p>
+          </div>
         </section>
       </div>
     );
