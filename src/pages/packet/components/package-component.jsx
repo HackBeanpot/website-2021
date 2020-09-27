b57ada27dfe84268eb1f1c6f5222846f7f2cb9da
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2020-09-27 19:29:25 -0400

    Convert to functional components and stuff.

--- src/pages/packet/components/package-component.jsx
+++ src/pages/packet/components/package-component.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
-class PackageComponent extends React.Component {
-  render() {
-    const perks = this.props.perks
+const PackageComponent = (props) => {
+    const {perks, level, trail} = props
     return (
-      <div className={`${this.props.trail}-${this.props.level}`}>
-        <ul className={`${this.props.trail}-${this.props.level}-list`}>
+      <div className={`package-component-${trail}-${level}`}>
+        <ul className={`${trail}-${level}-list`}>
           {perks.map((perk, index) => {
             return (
-              <p key={`${this.props.trail}-${this.props.level}-list-${index}`}> {perk} </p>
+              <p key={`${trail}-${level}-list-${index}`}> {perk} </p>
             )
           })}
         </ul>
       </div>
     )
-  }
 }
 
 export default PackageComponent;
