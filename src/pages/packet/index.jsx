b9572f00cc397b4eed8825e1e84f05b03b144731
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2020-10-03 19:48:02 -0400

    Nupur and Daniel learn how react hooks work and make the final price display properly.

--- src/pages/packet/index.jsx
+++ src/pages/packet/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import CloudBackground from '../../images/svg/packet-cloud.svg'
 import PacketStruct from './packet-structure.json'
 import PackageComponent from './components/package-component'
@@ -8,7 +8,22 @@ export default () => (
 )
 
 const SponsorPacket = () => {
-    return (
+  const [build, setBuild] = useState(0);
+  const [engage, setEngage] = useState(0);
+  const [recruit, setRecruit] = useState(0);
+
+  // TODO: make this not just an if statement lol
+  const setTrail = (level, trailType) => {
+    if (trailType === 'build') {
+      setBuild(level)
+    } else if (trailType === 'engage') {
+      setEngage(level)
+    } else if (trailType === 'recruit') {
+      setRecruit(level)
+    }
+  }
+
+  return (
       <div className="packet">
         <section className="packet-cover">
           <div className='title'>
@@ -32,7 +47,7 @@ const SponsorPacket = () => {
           {Object.keys(PacketStruct).map(trail => {
             const row = []
             {PacketStruct[trail].forEach((level, index) => {
-              row.push(<PackageComponent trail={trail} level={index + 1} perks={level}/>)
+              row.push(<PackageComponent trail={trail} level={index + 1} perks={level} callback={(level, trailType) => setTrail(level, trailType)}/>)
             })}
 
             return (
@@ -40,6 +55,7 @@ const SponsorPacket = () => {
             )
           })}
         </section>
+        <p> {build * 500 + recruit * 500 + engage * 500}</p>
       </div>
     );
 }
