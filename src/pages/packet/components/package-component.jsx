f012a6e96cc15257341667f9734d9bf47361cef1
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2020-10-25 16:24:20 -0400

    fix position of price label.

--- src/pages/packet/components/package-component.jsx
+++ src/pages/packet/components/package-component.jsx
@@ -4,9 +4,9 @@ const PackageComponent = (props) => {
     const {perks, level, trail, callback} = props
     return (
       <div className={`package-component-${trail}-${level}`}>
-        <div className={`package-component-${trail}-${level}-top-line`}>
-          <img src={require(`../../../images/svg/price-label-level-${level}.svg`)} alt='price-label' height="67" width="176" />
-          <input type="radio" name={trail} id={`${trail}-${level}-button`} onChange={() => callback(level, trail)}/>
+        <div className={`package-component-${trail}-${level}-top`}>
+          <img className={`${trail}-${level}-price-label`} src={require(`../../../images/svg/price-label-level-${level}.svg`)} alt='price-label' height="67" width="176" />
+          <input type="radio" className={`${trail}-${level}-radio`} id={`${trail}-${level}-button`} onChange={() => callback(level, trail)}/>
         </div>
         <ul className={`list-${trail}-${level}`}>
           {perks.map((perk, index) => {
