2d1f4db20ba77f361728477695142dba7613bf09
snwessel <wessel.s@husky.neu.edu>
2020-10-26 10:42:44 -0400

    Remove imports that are no longer needed

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import Helmet from 'react-helmet';
 
 import Layout from '../components/layout';
 import Welcome from '../components/sections/welcome';
 import ThemeAnnouncement from '../components/sections/theme-announcement';
-
-import favicon from './favicon.png';
 
 export default () => (
   <Layout>
