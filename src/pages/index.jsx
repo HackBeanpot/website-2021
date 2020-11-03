3da900f4471766a383c8303efff9c4c60ecca2c1
Zefeng (Daniel) Wang <wang.zef@husky.neu.edu>
2020-11-02 19:17:13 -0500

    Fix styling based on PR.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,10 +3,6 @@ import React from 'react';
 import Layout from '../components/layout';
 import Welcome from '../components/sections/welcome';
 import ThemeAnnouncement from '../components/sections/theme-announcement';
-import FAQItems from 'components/faqItems/faq-items';
-import AboutItem from 'components/aboutItem/about-item';
-import AboutContent from 'data/about-content.json';
-import Sponsors2020 from 'components/sponsors-2020'
 
 export default () => (
   <Layout>
