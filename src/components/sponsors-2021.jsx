23bb8fc6ca053d194eb3fadad0c5e3c80473d818
Sarah Wessel <snwessel@gmail.com>
2021-02-02 13:55:27 -0500

    Sarah sponsors (#27)
    * Add basic sponsors section to the website
    
    * Clean up CSS for sponsors section
    
    * Add glimpse logo + final fixes

---
+++ src/components/sponsors-2021.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+const Sponsors2021 = () => (
+  <div className="home-sponsors">
+    <div className="container">
+      <h2 className="home-sponsors__title">2021 Sponsors</h2>
+      {/* Facebook and Khoury */}
+      <div className="row">
+        <a
+          href="https://www.facebook.com/"
+          className="col-md-3 offset-md-2 home-sponsors__logo-wrapper my-sm-3 mx-5"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="home-sponsors__logo"
+            src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/facebook_2133px.png"
+            alt="Facebook logo"
+          />
+        </a>
+        <a
+          href="https://www.khoury.northeastern.edu/"
+          className="col-md-6 offset-md-1 home-sponsors__logo-wrapper my-sm-3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="home-sponsors__logo"
+            src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/khoury_1304px.png"
+            alt="Khoury logo"
+          />
+        </a>
+      </div>
+      
+      {/* In-Kind Sponsors */}
+      <h2 className="home-sponsors__subtitle">In-Kind Sponsors</h2>
+      <div className="row">
+        <div className="col-md-2"></div>
+        <a
+          href="https://www.badabeansnacks.com/"
+          className="col home-sponsors__logo-wrapper my-sm-3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="home-sponsors__logo"
+            src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/badabeanbadaboom_1500px.png"
+            alt="Facebook logo"
+          />
+        </a>
+        <a
+          href="https://www.joinglimpse.com/"
+          className="col home-sponsors__logo-wrapper my-sm-3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="home-sponsors__logo"
+            src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/glimpse.png"
+            alt="Facebook logo"
+          />
+        </a>
+        <div className="col-md-2"></div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Sponsors2021;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,6 +5,7 @@ import Welcome from '../components/sections/welcome';
 import ThemeAnnouncement from '../components/sections/theme-announcement';
 import FAQ from '../components/sections/faq';
 import AboutItem from "../components/aboutItem/about-item";
+import Sponsors2021 from 'components/sponsors-2021';
 
 export default () => (
   <Layout>
@@ -20,6 +21,7 @@ const App = () => {
         <ThemeAnnouncement />
         <AboutItem/>
         <FAQ />
+        <Sponsors2021 />
       </div>
     </div>
   );
