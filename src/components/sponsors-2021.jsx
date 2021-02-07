9291a6a91e63b9b6f8e74c2494f6d9dba7dbda89
snwessel <wessel.s@husky.neu.edu>
2021-02-07 13:48:54 -0500

    Add new logos, fix medium sizing

--- src/components/sponsors-2021.jsx
+++ src/components/sponsors-2021.jsx
@@ -4,6 +4,22 @@ const Sponsors2021 = () => (
   <section id="sponsors" className="home-sponsors">
     <div className="container">
       <h2 className="home-sponsors__title">2021 Sponsors</h2>
+      {/* Drift */}
+      <div className="row">
+        <a
+          href=""
+          className="col-md-6 offset-md-6 center home-sponsors__logo-wrapper my-sm-3 mx-5"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="home-sponsors__logo"
+            src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/drift.png"
+            alt="Facebook logo"
+          />
+        </a>
+      </div>
+
       {/* Facebook and Khoury */}
       <div className="row">
         <a
@@ -28,6 +44,35 @@ const Sponsors2021 = () => (
             className="home-sponsors__logo"
             src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/khoury_1304px.png"
             alt="Khoury logo"
+          />
+        </a>
+      </div>
+
+      
+      {/* PowerAdvocate and Upstatement */}
+      <div className="row">
+        <a
+          href=""
+          className="col-md-5 home-sponsors__logo-wrapper my-sm-3 mx-5"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="home-sponsors__logo"
+            src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/poweradvocate.png"
+            alt="Power Advocate logo"
+          />
+        </a>
+        <a
+          href=""
+          className="col-md-5 home-sponsors__logo-wrapper my-sm-3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="home-sponsors__logo"
+            src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/upstatment.png"
+            alt="Upstatement logo"
           />
         </a>
       </div>
