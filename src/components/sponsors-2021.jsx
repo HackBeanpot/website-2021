5bac0b4556f0b37085917b07d56ca467ea77ca0d
Sarah Wessel <snwessel@gmail.com>
2021-02-09 14:57:00 -0500

    Sarah new sponsors (#30)
    * Add new logos, fix medium sizing
    
    * Improve spacing for sponsor logos
    
    * Update sponsor links and order, add Zyprr
    
    * Update links

--- src/components/sponsors-2021.jsx
+++ src/components/sponsors-2021.jsx
@@ -4,11 +4,57 @@ const Sponsors2021 = () => (
   <section id="sponsors" className="home-sponsors">
     <div className="container">
       <h2 className="home-sponsors__title">2021 Sponsors</h2>
-      {/* Facebook and Khoury */}
+      {/* Drift */}
+      <div className="row">
+        <div className="col-md-4"></div>
+        <a
+          href="https://www.drift.com/"
+          className="col-md-4 home-sponsors__logo-wrapper mx-4"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="home-sponsors__logo"
+            src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/drift.png"
+            alt="Drift logo"
+          />
+        </a>
+      </div>
+      
+      {/* PowerAdvocate and Upstatement */}
       <div className="row">
         <a
+          href="https://w3.poweradvocate.com/"
+          className="col-md-5 home-sponsors__logo-wrapper mx-5"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="home-sponsors__logo"
+            src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/poweradvocate.png"
+            alt="Power Advocate logo"
+          />
+        </a>
+        <a
+          href="https://upstatement.com/"
+          className="col-md-5 home-sponsors__logo-wrapper"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="home-sponsors__logo"
+            src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/upstatement.png"
+            alt="Upstatement logo"
+          />
+        </a>
+      </div>
+
+      {/* Facebook and Khoury */}
+      <div className="row">
+        <div className="col-md-2"></div>
+        <a
           href="https://www.facebook.com/"
-          className="col-md-3 offset-md-2 home-sponsors__logo-wrapper my-sm-3 mx-5"
+          className="col-md-3 home-sponsors__logo-wrapper"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -20,7 +66,7 @@ const Sponsors2021 = () => (
         </a>
         <a
           href="https://www.khoury.northeastern.edu/"
-          className="col-md-6 offset-md-1 home-sponsors__logo-wrapper my-sm-3"
+          className="col-md-6 home-sponsors__logo-wrapper"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -35,7 +81,7 @@ const Sponsors2021 = () => (
       {/* In-Kind Sponsors */}
       <h2 className="home-sponsors__subtitle">In-Kind Sponsors</h2>
       <div className="row">
-        <div className="col-md-2"></div>
+        <div className="col-md-1"></div>
         <a
           href="https://www.badabeansnacks.com/"
           className="col home-sponsors__logo-wrapper my-sm-3"
@@ -45,7 +91,7 @@ const Sponsors2021 = () => (
           <img
             className="home-sponsors__logo"
             src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/badabeanbadaboom_1500px.png"
-            alt="Facebook logo"
+            alt="Bada Bean Bada Boom logo"
           />
         </a>
         <a
@@ -57,10 +103,22 @@ const Sponsors2021 = () => (
           <img
             className="home-sponsors__logo"
             src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/glimpse.png"
-            alt="Facebook logo"
+            alt="Glimpse logo"
           />
         </a>
-        <div className="col-md-2"></div>
+        <a
+          href="https://www.zipperhq.com/"
+          className="col home-sponsors__logo-wrapper my-sm-3"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="home-sponsors__logo"
+            src="https://tools.hackbeanpot.com/assets/logos/2021-sponsors/zipperHQ.png"
+            alt="ZipperHQ logo"
+          />
+        </a>
+        <div className="col-md-1"></div>
       </div>
     </div>
   </section>
