9d0a208068558e86e93a2b69dd24cd7f630e3942
Sarah Wessel <snwessel@gmail.com>
2021-03-11 09:16:46 -0500

    Replace the current header text with the core app (#31)

--- src/components/sections/welcome.jsx
+++ src/components/sections/welcome.jsx
@@ -5,11 +5,14 @@ import DaytimeScene from 'images/svg/daytime-scene.jsx';
 const Welcome = () => (
     <section className="header">
         <div className="container header__content">
-            <h1 className="header__title">HackBeanpot will be back!</h1>
-            <h2 className="header__event-date">February 19-21, 2021</h2>
-            <p className="header__description">Join us for our upcoming virtual hackathon!</p>
-            <a href='schedule.pdf' role="button" className="header__cta" target="_blank" rel="noopener noreferrer">
-            See this year’s schedule
+            <h1 className="header__title">HackBeanpot 2022</h1>
+            <h4 className="header__description">Join our organizing team!</h4>
+            <a href='https://docs.google.com/forms/d/e/1FAIpQLSefJBzPmcBjenKsfOmup16hra1v3XHasPsDENbx_K3CPdeUOQ/viewform' 
+                role="button" 
+                className="header__cta" 
+                target="_blank" 
+                rel="noopener noreferrer">
+            Apply Here
             </a>
         </div>
         <div className="header__skyline">
