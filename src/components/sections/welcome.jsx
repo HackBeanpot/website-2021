2a5b88e33122f0538e0a20f24aac658c61e421ce
snwessel <wessel.s@husky.neu.edu>
2021-03-27 11:17:45 -0400

    Update wording

--- src/components/sections/welcome.jsx
+++ src/components/sections/welcome.jsx
@@ -6,7 +6,7 @@ const Welcome = () => (
     <section className="header">
         <div className="container header__content">
             <h1 className="header__title">HackBeanpot 2022</h1>
-            <h4 className="header__description">Sign up for our Newsletter!</h4>
+            <h4 className="header__description">Join our mailing list to stay updated on our future adventures!</h4>
             <a href='https://hackbeanpot.us10.list-manage.com/subscribe?u=a98050d47fdae2481521f0474&id=dccd8c8431' 
                 role="button" 
                 className="header__cta" 
