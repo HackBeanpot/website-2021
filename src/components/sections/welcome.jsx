6863dbaca8177b33cf1fc870224fffd3a7e163c4
Sarah Wessel <snwessel@gmail.com>
2021-03-29 13:00:16 -0400

    Change the header back to the newsletter signup (#33)
    * Change the header back to the newsletter signup
    
    * Update wording

--- src/components/sections/welcome.jsx
+++ src/components/sections/welcome.jsx
@@ -6,13 +6,13 @@ const Welcome = () => (
     <section className="header">
         <div className="container header__content">
             <h1 className="header__title">HackBeanpot 2022</h1>
-            <h4 className="header__description">Join our organizing team!</h4>
-            <a href='https://docs.google.com/forms/d/e/1FAIpQLSefJBzPmcBjenKsfOmup16hra1v3XHasPsDENbx_K3CPdeUOQ/viewform' 
+            <h4 className="header__description">Join our mailing list to stay updated on our future adventures!</h4>
+            <a href='https://hackbeanpot.us10.list-manage.com/subscribe?u=a98050d47fdae2481521f0474&id=dccd8c8431' 
                 role="button" 
                 className="header__cta" 
                 target="_blank" 
                 rel="noopener noreferrer">
-            Apply Here
+            Sign up Here
             </a>
         </div>
         <div className="header__skyline">
